e55dbabc7efe093ffb8e8636a06c5e4de214968d
Botake <qbshi@princeton.edu>
2021-10-09 14:22:02 -0400

    Basic setup

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,10 @@
 from .views import *
+from django.urls import path
 
 app_name = 'main'
 
-urlpatterns = []
+urlpatterns = [
+    path('', HomePage, name='home'),
+    path('assignment', AssignmentPage, name='assignment'),
+    path('leaderboard', LeaderBoardPage, name='leaderboard'),
+]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+def HomePage(request):
+    context = {}
+    return render(request, 'main/home.html', context)
+
+def AssignmentPage(request):
+    context = {}
+    return render(request, 'main/assignment.html', context)
+
+def LeaderBoardPage(request):
+    context = {}
+    return render(request, 'main/leaderboard.html', context)
