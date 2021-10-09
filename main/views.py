d8233e7bd68b8334eeb5569e0bc2491e7f7b9c39
kharnnnn <nigamkharn@gmail.com>
2021-10-09 17:29:16 -0400

    tried to make variable for token and name - Kharn

--- main/views.py
+++ main/views.py
@@ -3,7 +3,10 @@ from .models import *
 
 # Create your views here.
 def HomePage(request):
-    context = {}
+    context = {
+        'name'='ben shi'
+        'token' = '5'
+    }
     return render(request, 'main/home.html', context)
 
 def AssignmentPage(request):
