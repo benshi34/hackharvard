ecb8d73d1c3824a75d008ee30956cdf2d962d515
Botake <qbshi@princeton.edu>
2021-10-09 18:28:09 -0400

    asdfasd

--- main/urls.py
+++ main/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', HomePage, name='home'),
     path('assignment/', QuizListView.as_view(), name='assignment'),
     path('assignment/<pk>/', AssignmentPage, name='assignment-view'),
+    path('assignment/<pk>/data/', assignment_data_view, name='assignment-data-view'),
     path('leaderboard/', LeaderBoardPage, name='leaderboard'),
 ]
--- main/views.py
+++ main/views.py
@@ -19,6 +19,19 @@ def AssignmentPage(request, pk):
     context = {'obj': quiz}
     return render(request, 'main/assignment.html', context)
 
+def assignment_data_view(request, pk):
+    quiz = Quiz.objects.get(pk=pk)
+    questions = []
+    for q in quiz.get_questions():
+        answers = []
+        for a in q.get_answers():
+            answers.append(a.text)
+        questions.append({str(q): answers})
+    return JsonResponse({
+        'data': questions,
+        'time': quiz.time,
+    })
+
 def LeaderBoardPage(request):
     context = {}
     return render(request, 'main/leaderboard.html', context)
