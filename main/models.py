999a2cc59fd55a399b8922bf3072f2f2861589e7
Botake <qbshi@princeton.edu>
2021-10-09 14:44:53 -0400

    Moved stuff and modified html

--- main/models.py
+++ main/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Quiz(models.Model):
+    name = models.CharField(max_length=120)
+    topic = models.CharField(max_length=120)
+    number_of_questions = models.IntegerField()
