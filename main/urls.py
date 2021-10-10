d4cdd94243b4cf4fb30b17a938c46e8ad02518f7
Botake <qbshi@princeton.edu>
2021-10-09 23:50:55 -0400

    asdfsadf

--- main/urls.py
+++ main/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('assignment/<pk>/', AssignmentPage, name='assignment-view'),
     path('assignment/<pk>/data/', assignment_data_view, name='assignment-data-view'),
     path('leaderboard/', LeaderBoardPage, name='leaderboard'),
+    path('assignment/<pk>/save/', save_quiz_view, name='save-view'),
 ]
